01ad3f5a2bfcf95eb5afd99228dfd42fe14d37e1
Stéphane FRACHE <stephane.frache@keyconsulting.fr>
2022-03-16 14:03:09 +0100

    Removed unused usings

--- TestCaseBuilder/BusinessObjects/Customer.cs
+++ TestCaseBuilder/BusinessObjects/Customer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestCaseBuilder.BusinessObjects
+﻿namespace TestCaseBuilder.BusinessObjects
 {
     public class Customer
     {
